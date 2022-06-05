e90a90c8c6e1aab537daa5e33a5617eb3f4c61d8
Noman <noman.khan3701@gmail.com>
2022-06-05 15:18:30 +0530

    Added for production

--- server/index.js
+++ server/index.js
@@ -5,17 +5,26 @@ const app = express();
 const userRoute = require("./routes/user");
 const postRoute = require("./routes/post");
 const commentRoute = require("./routes/comment");
-const cors = require("cors");
+// const cors = require("cors");
 
 dotenv.config();
 const PORT = process.env.PORT || 8000;
-const corsOptions = {
-  origin: "*",
-};
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.listen(PORT, async () => {
+  try {
+    await mongoose.connect(
+      process.env.MONGO_URL,
+      { useNewUrlParser: true },
+      () => console.log(`Server running at port ${PORT}`)
+    );
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 app.use("/api/user", userRoute);
 app.use("/api/post", postRoute);
@@ -23,14 +32,4 @@ app.use("/api/comment", commentRoute);
 
 app.get("/", (req, res) => {
   res.send("Hallaluya");
-});
-
-app.listen(PORT, () => {
-  try {
-    mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () =>
-      console.log(`Server running at port ${PORT}`)
-    );
-  } catch (e) {
-    console.log(e);
-  }
 });
